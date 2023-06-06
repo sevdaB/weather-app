88b5a71f51912a7d0520e58dc17774463d957ae1
sevdaB <sevdabafandeh@gmail.com>
2023-06-06 11:38:41 +0430

    changed Api key

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "f5e814a04eddfab1740f07bf0328eee2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
