c4a5080c9de730d108def7b63dba6d96c48a1f79
sevdaB <sevdabafandeh@gmail.com>
2023-06-09 13:10:04 +0430

    I fixed Api

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "f5e814a04eddfab1740f07bf0328eee2";
+  let apiKey = "fda3688b1db05987dd5d07c237aecfba";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -100,7 +100,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "fda3688b1db05987dd5d07c237aecfba";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
